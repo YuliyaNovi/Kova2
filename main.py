0bb481b1ef5c6a40735601a89f71a2d4c6dfd940
Yuliya <fealka79@gmail.com>
2023-07-11 10:22:14 +0300

    Форма регистрации

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -159,6 +159,95 @@ def variants(var):
             </html>"""
     else:
         return "Я не знаю о чем вы!"
+
+@app.route('/slideshow')
+def slideshow():
+    return f"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Постер</title>
+        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+    <body>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{url_for('static', filename='images/spain1.jpg')}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{url_for('static', filename='images/spain2.jpg')}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{url_for('static', filename='images/spain3.jpg')}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+ <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+    </html>"""
+
+
+@app.route('/form_sample', methods=['GET', 'POST'])
+def form_sample():
+    if request.method == 'GET':
+        return f"""<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Пример формы</title>
+                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+            </head>
+            <body>
+            <h1>Форма для регистрации</h1>
+            <div class="container">
+            <form class="login_form" method="post">
+            <input type="text" class="form-control" name="fname" placeholder="Фамилия"><br>
+            <input type="text" class="form-control" name="sname" placeholder="Имя"><br>
+            <input type="text" class="form-control" name="email" placeholder="E-mail"><br>
+            <input type="text" class="form-control" name="password" placeholder="Password"><br>
+            <div class="form-group">
+            <label for="classSelect">Ваше образование</label>
+            <select class="form-control" id="classSelect" name="profession">
+            <option>Высшее</option>
+            <option>Среднее</option>
+            </select>
+            </div>
+            <!--Radio Button - Gender Selection -->
+            <div class="form-group">
+            <label for=form-check">Укажите пол</label> 
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+            <input class="form-check-label" for="male">Мужской</label>
+            </div>
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+            <input class="form-check-label" for="female">Женский</label>
+            </div>
+        </div>
+        <!-- End of Gender Selection -->    
+            <button type="submit" class="btn btn-primary">Отправить</button>
+            </form>
+            </div>
+            </body>
+            </html>"""
+    elif request.method == 'POST':
+        print(request.method)
+        print(request.form['fname'])
+        print(request.form['sname'])
+        return 'Форма отправлена'
+
 
 
 if __name__ == '__main__':
